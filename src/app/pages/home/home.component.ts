ac3902cbf1f3508fd43c7869c2784f9844f3b4a1
Nikolas <nickelpick12@gmail.com>
2021-06-03 10:15:06 -0700

    Updated file location of service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { ContentDbService } from '../backend/content-db.service';
+import { ContentDbService } from '../../backend/content-db.service';
 
 @Component({
   selector: 'app-home',
