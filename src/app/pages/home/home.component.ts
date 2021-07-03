c270b68092f51881b43fbca2251da73a8f1ab837
Nikolas <nickelpick12@gmail.com>
2021-07-03 16:22:23 -0700

    Setup Home Page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { BehaviorSubject } from 'rxjs';
+import { PageData } from 'src/app/backend/page-data';
+import { Section } from 'src/app/backend/section';
 import { ContentDbService } from '../../backend/content-db.service';
 
 @Component({
@@ -8,21 +11,43 @@ import { ContentDbService } from '../../backend/content-db.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  pageData = new BehaviorSubject<PageData>({
+    pageTitle: "",
+    sections: []
+  });
 
-  pageData: any = [];
+  // HTML Var
+  height: number;
 
   constructor(private contentDbService: ContentDbService, public domSanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
-    // this.contentDbService.getPageData("home").then(data => {
-    //   console.log(data);
-    //   this.pageData = data;
-    // })
+    this.loadPageData();
   }
 
-  // sanitize(url: string) {
-  //   console.log(url);
-  //   return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
-  // }
+  loadPageData() {
+    this.contentDbService.getPageData('home').subscribe(doc => {
+      var nextPageData: PageData = {
+        pageTitle: doc.pageTitle,
+        sections: doc.sections
+      }
+      this.pageData.next(nextPageData);
+    });
+  }
 
+  // HTML Functions
+  src(url: string) {
+    return `url(${url})`;
+  }
+
+  calcuateGridTemplateRows(section: Section) {
+    this.height = 1;
+    section.contentBoxes.forEach(contentBox => {
+      if(contentBox.yEnd > this.height) {
+        this.height = contentBox.yEnd;
+      }
+    });
+    this.height;
+    return `repeat(${this.height-1}, 8.58vw)`;
+  }
 }
